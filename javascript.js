42075869b57c6a9ba37ce02d2a58de514de1b710
Devops Nika <nikasano@protonmail.com>
2019-01-03 12:56:39 +0200

    Reverted to old version

--- javascript.js
+++ javascript.js
@@ -1,6 +1,2 @@
 console.log("Hello World");
 console.log("Hello Another World");
-
-console.log("Just another console.log");
-console.log("This was added as version1 branch");
-console.log("Another change");
