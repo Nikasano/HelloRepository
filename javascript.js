f945d78c44c1cccf91ed5f245316c87e5747d7a5
Devops Nika <nikasano@protonmail.com>
2019-01-03 13:16:22 +0200

    Remerge

--- javascript.js
+++ javascript.js
@@ -1,9 +1,2 @@
 console.log("Hello World");
 console.log("Hello Another World");
-<<<<<<< HEAD
-=======
-
-console.log("Just another console.log");
-console.log("This was added as version1 branch");
-console.log("Another change");
->>>>>>> parent of 4207586... Reverted to old version
